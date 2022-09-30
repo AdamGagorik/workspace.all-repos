51457966bdcb59e5c26069b18465eafb8ce2ea1a
Adam Gagorik <adam.gagorik@gmail.com>
2022-09-30 10:09:40 -0400

    Move main logic into base class

--- autofix/fixers/base.py
+++ autofix/fixers/base.py
@@ -1,4 +1,6 @@
+import argparse
 import dataclasses
+import logging
 
 
 @dataclasses.dataclass()
@@ -6,7 +8,25 @@ class Fixer:
     force: bool = False
 
     def apply(self):
-        pass
+        ...
 
     def check(self, **kwargs):
-        pass
+        ...
+
+    @classmethod
+    def args(cls) -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser(description=cls.__doc__)
+        parser.add_argument("--force", action="store_true")
+        return parser
+
+    @classmethod
+    def main(cls, argv=None):
+        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        opts = cls.args().parse_args(args=argv)
+        fixer = cls(force=opts.force)
+        fixer.apply()
+        fixer.check()
+
+
+if __name__ == "__main__":
+    raise SystemExit(Fixer.main())

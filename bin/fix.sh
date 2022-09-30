2282ba6930d5560d373619a46b76f47820d8cba1
Adam Gagorik <adam.gagorik@gmail.com>
2022-09-30 10:09:50 -0400

    Add fix script

---
+++ bin/fix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+if [ $# -eq 0 ]
+  then
+    echo "Expecting the name of the fixer to apply!"
+    exit 1
+fi
+# shellcheck source=/dev/null
+source ~/.local/pipx/venvs/all-repos/bin/activate
+python -m autofix.fixers.$1 "${@:2}"

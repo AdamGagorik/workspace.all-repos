d550381b6a8950691b0729f81998d15a7c7c4e29
Adam Gagorik <adam.gagorik@gmail.com>
2022-10-03 09:16:14 -0400

    Make sure fixer arguments are correctly populated

--- autofix/fixers/base.py
+++ autofix/fixers/base.py
@@ -14,15 +14,18 @@ class Fixer:
         ...
 
     @classmethod
-    def args(cls) -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser(description=cls.__doc__)
-        parser.add_argument("--force", action="store_true")
-        return parser
+    def args(cls, **kwargs):
+        parser = argparse.ArgumentParser(
+            **(dict(usage=f"python -m {cls.__module__}", formatter_class=argparse.RawDescriptionHelpFormatter) | kwargs)
+        )
+        group = parser.add_argument_group(f"{cls.__module__}")
+        group.add_argument("--apply-changes", dest="force", action="store_true", help="actually apply changes?")
+        return group, parser
 
     @classmethod
     def main(cls, argv=None):
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-        fixer = cls(options=cls.args().parse_args(args=argv))
+        fixer = cls(options=cls.args()[-1].parse_args(args=argv))
         fixer.apply()
         fixer.check()
 
--- autofix/fixers/syncfile.py
+++ autofix/fixers/syncfile.py
@@ -1,33 +1,41 @@
-import argparse
 import logging
 import os.path
+import pathlib
 import shutil
+import subprocess
 
 from .base import Fixer as Base
 
 
 class Fixer(Base):
     def apply(self):
-        for inp_path, out_path in map(lambda v: v.split(":"), self.options.sync):
+        assert len(self.options.src) == len(self.options.dst), "inputs not in pairs"
+
+        for inp_path, out_path in zip(self.options.src, self.options.dst):
+            inp_path = inp_path.absolute()
+            out_path = out_path.absolute()
+
             if not os.path.exists(inp_path):
                 raise FileNotFoundError(inp_path)
 
-            if not os.path.exists(out_path) and not self.options.create:
+            if not os.path.exists(out_path) and not self.options.allow_missing:
                 logging.error("[SKIP]: out path does not exist! use --create : %s", out_path)
                 continue
 
             if self.options.force:
                 logging.info("cp %s %s", inp_path, out_path)
                 shutil.copy(inp_path, out_path)
+                subprocess.check_call(["git", "add", out_path])
             else:
                 logging.error("skipping logic! use --force : cp %s %s", inp_path, out_path)
 
     @classmethod
-    def args(cls) -> argparse.ArgumentParser:
-        parser = super().args()
-        parser.add_argument("--create", action="store_true", help="create file is missing?")
-        parser.add_argument("--sync", nargs="*", required=True, help="the file to sync")
-        return parser
+    def args(cls, **kwargs):
+        group, parser = super().args(**kwargs)
+        group.add_argument("--allow-missing", action="store_true", help="create file if missing?")
+        group.add_argument("--src", nargs="*", metavar="SRC", type=lambda p: pathlib.Path(p).absolute(), help="input")
+        group.add_argument("--dst", nargs="*", metavar="DST", type=pathlib.Path, help="output")
+        return group, parser
 
 
 if __name__ == "__main__":
